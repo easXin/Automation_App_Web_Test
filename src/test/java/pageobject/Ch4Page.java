3b60c70d2a0c770dde8cf7164f2fedd044d1fb78
easXin <codingericwang@gmail.com>
2020-03-10 16:17:59 -0700

    finish ch4 page

---
+++ src/test/java/pageobject/Ch4Page.java
@@ -0,0 +1,77 @@
+package pageobject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+class Ch4Page{
+	WebDriver webDriver = null;
+	@FindBy(xpath="//a[@id='index']")
+	WebElement goBackToPreviousPage;
+	
+	@FindBy(xpath="//input[@id='dateInput']")
+	WebElement firstInputField;
+	
+	@FindBy(xpath="//select[@id='selecttype']")
+	WebElement dropDownMenu1;
+	
+	@FindBy(xpath="//input[@id='selectLoad']")
+	WebElement thisTextFieldDoingNothing;
+	
+	@FindBy(xpath="//select[@id='ajaxLoad']")
+	WebElement dropDownMenu2_doingNothing;
+	
+	@FindBy(xpath="//input[@id='blurry']")
+	WebElement secondInputField;
+	
+	@FindBy(xpath="//input[@id='nextBid']")
+	WebElement thirdInputField;
+	
+	@FindBy(xpath="//div[@id='hoverOver']")
+	WebElement hoverArena;
+	
+	public Ch4Page() {}
+	public Ch4Page(WebDriver webDriver){
+		this.webDriver = webDriver;
+	}
+	public void goToHomePage() {
+		goBackToPreviousPage.click(); // and come back to this page
+	}
+	public void enterSomeText1() {
+		firstInputField.sendKeys("Hello");
+	}
+	public void dropDownMenu1() {
+		Select option = new Select(dropDownMenu1);
+		// IDE Core RC Grid
+		for (int i = 0; i < 4; i++) {
+			try {
+				Thread.sleep(4000);
+				option.selectByIndex(i);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			option.selectByIndex(i);
+		}
+	}
+	public void clickOnThisTextField() {
+		thisTextFieldDoingNothing.click();
+	}
+	public void dropDownMenu2() {
+		Select option = new Select(dropDownMenu1);
+		option.equals(null);
+	}
+	public void enterSomeText2() {
+		secondInputField.sendKeys("Wassup");
+	}
+	public void enterSomeText3() {
+		thirdInputField.sendKeys("Hello from the other side");
+	}
+	public void dismissAlertWindows() {
+		Actions builder = new Actions(webDriver);
+		builder.moveToElement(hoverArena).perform();
+		webDriver.switchTo().alert().accept();
+	}
+}
