e334c25b42d05ef2982c633fe1abbeab64229e73
easXin <codingericwang@gmail.com>
2020-03-10 18:37:55 -0700

    clean up the code, add testNG dependency

--- src/test/java/runner/WebRunner.java
+++ src/test/java/runner/WebRunner.java
@@ -14,20 +14,5 @@ import io.github.bonigarcia.wdm.WebDriverManager;
     glue = "stepDefinitions",
     tags = "@test")
 public class WebRunner {
-	public static void main(String[] args) {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
-		driver.close();
-		driver.quit();
-	}
-	
-/*
- * Create steps in the feature files and relate them to step definitions 
- * so that during your test you open some page, fill in some text fields,
- * press some button, check checkbox, click some link. (the main goal is
- * to practice feature files, step definitions, interaction with various
- * elements on the page) - do as much as you can...a little exploration
- * here.
- */
+
 }
